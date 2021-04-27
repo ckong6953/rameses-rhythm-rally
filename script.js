7eeea8d7d1d8bfa81946b53abe77b7b67ad0ef75
ckong6953 <ckong6953@gmail.com>
2021-04-27 02:55:57 -0400

    Add intial workings of animation

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // The function called to render the main menu for the game.
-const renderMainMenu = function(){
-    const mainMenu = 
-    `<div class="card" id="main-menu">
+const renderMainMenu = function () {
+    const mainMenu =
+        `<div class="card" id="main-menu">
         <header class="card-header>
             <p class="card-header-title">
                 Rameses' Rhythm Rally
@@ -10,7 +10,7 @@ const renderMainMenu = function(){
         <div class="card-content" id="main-menu-content">
             <div class="content">
                 <button class="button main-menu-button" id="play-button">Play Game</button>
-                <button class="button main-menu-button"id="leaderboard-button">Leaderboard</button>
+                <button class="button main-menu-button" id="leaderboard-button">Leaderboard</button>
                 <button class="button main-menu-button" id="about-button">About</button>
             </div>
         </div>
@@ -18,9 +18,10 @@ const renderMainMenu = function(){
     return mainMenu;
 }
 
-const renderDescription = function (){
-    const description = 
-    `<div class="card-content" id="main-menu-content">
+// This is function that renders the description/controls of the game. 
+const renderDescription = function () {
+    const description =
+        `<div class="card-content" id="main-menu-content">
         <div class="content">
             <p> It’s the night of the big game, and Rameses needs to get the crowd pumped up! Hit the arrow keys on rhythm to get the best score! </p>
             <button class="button main-menu-button" id="back-button">Back</button>
@@ -33,19 +34,53 @@ const renderDescription = function (){
 
 
 // This is the main function that renders the initial game and handles all event handlers. 
-const loadGame = function(){
+const loadGame = function () {
     const $root = $("#root");
     $root.append(renderMainMenu());
-    // $root.on("click","#play-button", renderGame);
-    $root.on("click","#about-button", renderDescription);
-    $root.on("click", "#back-button", function(){
+    $root.on("click", "#play-button", renderGame);
+    $root.on("click", "#about-button", renderDescription);
+    $root.on("click", "#back-button", function () {
         $('#main-menu').replaceWith(renderMainMenu());
     });
     // $root.on("click","#leaderboard-button", renderLeaderboard);
 }
 
+// When the user would like to play game, this renders the initial state of the game as in the board and the notes to be played.
+const renderGame = function () {
+    const gameState =
+    `<div class = "game-container">
+        <div class = "key-container">
+        </div>
+        <div class = "track-container">
+        </div>
+    </div>`;
+    $("#main-menu").replaceWith(gameState);
+    renderNotes();
+}
+
+const renderNotes = function () {
+    song.tracks.forEach(function (track, trackIndex) {
+        const $addTrack =
+            $(`<div class="track" id="track-${trackIndex}">
+            </div>`);
+        track.notes.forEach(function(note, noteIndex){
+            const $addNote =
+            $(`<div class="note" id="note-${noteIndex}"></div>`);
+            $addNote.css({
+                "background-color": `${track.color}`,
+                "animation-name": "moveUp",
+                "animation-timing-function": "linear",
+                "animation-duration": `${note.duration}` + "s",
+                "animation-delay": `${note.delay}` + "s",
+                "animation-play-state": "running",
+            });
+            $addTrack.append($addNote);
+        });
+        $(".track-container").append($addTrack);
+    });
+}
 
 // On window load, this will render the initial state. 
-$(function (){
+$(function () {
     loadGame();
 });
---
+++ song.js
@@ -0,0 +1,52 @@
+const left = {
+    color: '#C24B99',
+    nextNote: 0,
+    notes: [
+        {duration: 3, delay: 1},
+        {duration: 3, delay: 2},
+        {duration: 3, delay: 3},
+        {duration: 3, delay: 4},
+        {duration: 3, delay: 5},
+    ]
+};
+
+const up = {
+    color: '#13FA05',
+    nextNote: 0,
+    notes: [
+        {duration: 3, delay: 1},
+        {duration: 3, delay: 2},
+        {duration: 3, delay: 3},
+        {duration: 3, delay: 4},
+        {duration: 3, delay: 5},
+    ]
+}
+
+const down = {
+    color: '#01FFFF',
+    nextNote: 0,
+    notes: [
+        {duration: 3, delay: 1},
+        {duration: 3, delay: 2},
+        {duration: 3, delay: 3},
+        {duration: 3, delay: 4},
+        {duration: 3, delay: 5},
+    ]
+}
+
+const right = {
+    color: '#F9393F',
+    nextNote: 0,
+    notes: [
+        {duration: 3, delay: 1},
+        {duration: 3, delay: 2},
+        {duration: 3, delay: 3},
+        {duration: 3, delay: 4},
+        {duration: 3, delay: 5},
+    ]
+};
+
+const song  = {
+    duration: 20,
+    tracks: [left, up, down, right],
+};
